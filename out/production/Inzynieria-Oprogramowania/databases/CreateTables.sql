1c71ca284b1745d2fdd7bca8d389fbcc03d3b05a
Oleksii <alessytnik01@gmail.com>
2022-06-02 08:14:22 +0200

    hello

---
+++ out/production/Inzynieria-Oprogramowania/databases/CreateTables.sql
@@ -0,0 +1,46 @@
+DROP TABLE if exists UsersData;
+drop table if exists UsersAccounts;
+DROP TABLE if exists Users;
+
+create table Users(
+username varchar(30) not NULL PRIMARY key,
+`password` varchar(30) not null,
+email varchar(30)
+);
+
+create table UsersData(
+username varchar(30) NOT NUll,
+`First name` varchar(20),
+`Last name` varchar(20),
+`Sex` varchar(1),
+City varchar(20),
+Address varchar(30),
+PESEL varchar(30),
+foreign KEY(username) REFERENCES Users(username)on update cascade on delete cascade
+);
+
+create table UsersAccounts(
+username varchar(30) not null,
+`Ordinary account number` varchar(30),
+`Savings account number` varchar(30),
+foreign key(username) references Users(username) on update cascade on delete cascade
+);
+
+/*create table Credits(
+id int not null,
+username varchar(30) not null,
+amount int,
+`start date` date,
+`month payment` int,
+`rate` float,
+
+
+);*/
+
+/*create table UsersCards(
+username varchar(30) not null,
+);*/
+
+
+
+
