7688e0df68a6a3753f9c3aa482d97e18a5c00e96
max586 <maks.ovsienko2@gmail.com>
2022-06-06 16:31:06 +0200

    duplicated windows bug solved

---
+++ tests/src/AuthenticationAndRegistration/AuthenticationScreenTest.java
@@ -0,0 +1,10 @@
+package src.AuthenticationAndRegistration;
+
+import static org.junit.Assert.*;
+
+public class AuthenticationScreenTest {
+
+    @org.junit.Test
+    public void createScreen() {
+    }
+}
