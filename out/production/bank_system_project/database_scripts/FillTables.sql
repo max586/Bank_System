cf28bb9cc1cbf7cb025b677c6937952a0b38e50a
Oleksii <alessytnik01@gmail.com>
2022-06-09 01:39:38 +0200

    refactored

--- out/production/bank_system_project/database_scripts/FillTables.sql
+++ out/production/bank_system_project/database_scripts/FillTables.sql
@@ -6,7 +6,7 @@ insert INTO UsersData values
 ("test_user","fn","ln","M","27983719","somewhere","30-178","ul abc",125,"6381763182"),("user1","Max","Apol","M","3627318","Krakow","21-212","ul.ajk",90,"78022184"),("user2","Alex","Bos","M","9019201","Zakopane","10-010","ul.apoa",120,"480222354"),
 ("user3","Morty","Bonon","M","1209102","Warszawa","129-02","ul. pop",88,"51062893"),("user4","Sam","Camel","M","81323213","Bydgoszcz","89-908","ul. nmn", 11,"570308852"),("user5","Lila","Stich","F","13131321","Gdansk","21-210","ul. lasas", 99,"520829164");
 
-insert into OrdinaryAccounts values("test_user","PL666",1000),("user1","PL9987",999),("user2","PL1819",7878),("user3","PL12344",2000),("user4","PL8191",10000),("user5","UK127125",9090);
+insert into OrdinaryAccounts values("test_user","PL666",100000),("user1","PL9987",999),("user2","PL1819",7878),("user3","PL12344",2000),("user4","PL8191",10000),("user5","UK127125",9090);
 insert into SavingsAccounts values("test_user","PL999",10000,4),("user1","PL1212",5000,4),("user2","PL12122",300,4),("user3","PL1909",3000,4),("user4","PL9000",9000,4),("user5","UK125",11000,4);
 
 INSERT INTO Cards values("test_user","1298109280119","1234"),("user1","1919919191","1234");
