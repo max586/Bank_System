b07e47a97c09b10cc0c33387ea7584e5590beb1b
Oleksii <alessytnik01@gmail.com>
2022-06-09 10:26:43 +0200

    sth added

--- src/transfers/SavingsHistory.java
+++ src/transfers/SavingsHistory.java
@@ -12,7 +12,7 @@ import javax.swing.*;
 import java.sql.SQLException;
 
 public class SavingsHistory {
-        private JPanel ordinaryPanel;
+        private JPanel ordinaryPanel ;
         private JLabel panelTitleLabel;
         private JPanel timerPanel;
         private JLabel timeLabel;
