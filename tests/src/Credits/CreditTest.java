10ab2d8b62530aed76821a4783157efd3195f17c
max586 <maks.ovsienko2@gmail.com>
2022-06-12 22:50:26 +0200

    sth fixed

--- tests/src/Credits/CreditTest.java
+++ tests/src/Credits/CreditTest.java
@@ -25,13 +25,15 @@ public class CreditTest {
         user.savings_account_balance=1000;
         Database.addUser(user.username,user.password,user.email,"1234");
         Database.addOrdinaryAccountNumber(user.username,user.ordinary_account_number);
+        Database.setOrdinaryAccountBalance(user.username,user.ordinary_account_balance);
         Database.addSavingsAccountNumber(user.username,user.savings_account_number);
+        Database.setSavingsAccountBalance(user.username,user.savings_account_balance);
         screen = new Credit(user,null,null);
-        screen.frame.dispose();
+        //screen.frame.dispose();
     }
     @Test
     public void alreadyHaveCredit(){
-        Database.addCredit(user.username,1000,0,"2020-08-26",2);//add user credit
+        Database.addCredit(user.username,1000,0,"2020-06-10",2);//add user credit
         screen.takeNewCreditButton.doClick();
         assertEquals("You already have a credit",screen.jpane.getMessage());
     }
@@ -52,14 +54,22 @@ public class CreditTest {
         screen.Years.setText("1234.789");
         screen.takeNewCreditButton.doClick();
         assertEquals("Now you have the credit",screen.jpane.getMessage());
+        assertEquals(11000,screen.user.ordinary_account_balance,3);
+        Database.deleteCredit(user.username);
         screen.Amount.setText("1000");
         screen.Years.setText("123");
         screen.takeNewCreditButton.doClick();
         assertEquals("Now you have the credit",screen.jpane.getMessage());
+        assertEquals(12000,screen.user.ordinary_account_balance,3);
     }
     @Test
     public void checkDebt(){
-
+        Database.addCredit(user.username,1000,0,"2020-06-10",2);//add user credit
+        assertEquals(200,Float.parseFloat(screen.MyPayedCredit.getText()),3);
+        assertEquals(0,Float.parseFloat(screen.MyDebt.getText()),3);
+        screen.payDebtButton.doClick();
+        assertEquals(550,Float.parseFloat(screen.MyPayedCredit.getText()),3);
+        assertEquals(0,Float.parseFloat(screen.MyDebt.getText()),3);
     }
     @After
     public void deleteUser(){Database.deleteUser(user.username);}
