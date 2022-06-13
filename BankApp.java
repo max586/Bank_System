4648865929a416d93a9a478d0775d35bf6e65b34
max586 <maks.ovsienko2@gmail.com>
2022-06-13 10:15:31 +0200

    -refactored a little bit -execution instruction added

---
+++ BankApp.java
@@ -0,0 +1,9 @@
+
+import src.*;
+import src.AuthenticationAndRegistration.*;
+
+public class BankApp {
+    public static void main(String[] args) {
+        new AuthenticationScreen(null,null,new Screen()).CreateScreen();   
+    }
+}
